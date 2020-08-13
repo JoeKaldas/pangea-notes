df382a6141464609099e0e9042d0a0531d11e97e
Joe Kaldas <joe.kaldas@applab.co>
2020-08-13 18:37:01 +0200

    Feat: Create notes table with columns title/body/user_id

--- db/migrate/20200810160659_create_notes.rb
+++ db/migrate/20200810160659_create_notes.rb
@@ -1,7 +1,9 @@
 class CreateNotes < ActiveRecord::Migration[5.2]
   def change
     create_table :notes do |t|
-
+      t.string :title
+      t.text :body
+      t.references :user,	foreign_key: true
       t.timestamps
     end
   end
