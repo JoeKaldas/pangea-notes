3870c1df0837195388359d1fb6f2ca8be3c022c4
Joe Kaldas <joe.kaldas@applab.co>
2020-08-13 18:45:54 +0200

    Feat: Add standard controller actions (index, show, new, edit, destroy)

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,39 @@
 class NotesController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def set_note
+    @note = current_user.notes.find_by(id: params[:id])
+  end
+
 end
